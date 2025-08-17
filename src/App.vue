ad5a8eba9481fe5340c137621ac6821e849ba02d
zhongtao <ztappleemail@163.com>
2025-08-17 23:30:16 +0800

    feat(App): 添加 reactive 示例并测试整体赋值
    - 在 App.vue 中添加 reactive 示例
    - 新增 reactiveObj 变量和相关方法
    - 测试并演示 reactive 和 ref 的区别
    - 优化界面显示和按钮功能

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <h1 class="rootClass">根组件</h1>
   <h2>ref()->reactive():{{ refObj }}</h2>
-  <h2>reactive()专门处理数组和对象:{{ refObj }}</h2>
-  <button @click="changeRefObj">点击</button>
+  <button @click="changeRefObj">点击整体赋值</button>
+  <h2>reactive()无法整体赋值:{{ reactiveObj }}</h2>
+  <button @click="changeReactiveObj">点击整体赋值失效</button>
+  <button @click="changeReactiveValue">点击只能修改属性值</button>
 
   <MyChild fatherData="父数据">
     <template v-slot:childSlot1>
@@ -13,24 +15,44 @@
 
 <script>
   import MyChild from "./pages/MyChild.vue";
-  import {ref} from "vue";
+  import {reactive, ref} from "vue";
 
   export default {
     setup() {
-      let refObj = ref({name: "响应式数据src", array: [1, 2, 3]})
+      let refObj = ref({name: "响应式数据refObj", array: [1, 2, 3]})
+      let reactiveObj = reactive({name: "响应式数据reactiveObj", array: [1, 2, 3]})
 
       function changeRefObj() {
-        if (refObj.value.name === "响应式数据src") {
-          refObj.value = {name: "响应式数据change1", array: [4, 5, 6]}
+        if (refObj.value.name === "响应式数据refObj") {
+          refObj.value = {name: "响应式数据refObj->Change", array: [4, 5, 6]}
         } else {
-          refObj.value = {name: "响应式数据src", array: [1, 2, 3]}
+          refObj.value = {name: "响应式数据refObj", array: [1, 2, 3]}
         }
         console.log(refObj)
+      }
+      function changeReactiveObj() {
+        if (reactiveObj.name === "响应式数据reactiveObj") {
+          reactiveObj = {name: "响应式数据reactiveObj->Change", array: [4, 5, 6]}
+        } else {
+          reactiveObj = {name: "响应式数据reactiveObj", array: [1, 2, 3]}
+        }
+        console.log(refObj)
+      }
+      function changeReactiveValue() {
+        if (reactiveObj.name === "响应式数据reactiveObj") {
+          reactiveObj.name = "响应式数据reactiveObj->change"
+        } else {
+          reactiveObj.name = "响应式数据reactiveObj"
+        }
+        console.log(reactiveObj)
       }
 
       return {
         refObj,
-        changeRefObj
+        reactiveObj,
+        changeRefObj,
+        changeReactiveObj,
+        changeReactiveValue
       }
     },
     components: {
