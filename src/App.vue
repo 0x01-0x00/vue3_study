c7319308f567196a4eef4992aac29a8c290ad3a1
zhongtao <ztappleemail@163.com>
2025-08-18 13:28:20 +0800

    feat(App): 添加 toRef 示例并优化页面结构
    - 在 App.vue 中添加 toRef 示例，展示其使用方法
    - 优化页面结构，增加分隔线和调整标题层级
    - 引入 MyChild 组件，演示子组件插槽使用

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
+  <hr/>
   <h1 class="rootClass">根组件</h1>
-  <h2>ref()->reactive():{{ refObj }}</h2>
+  <h3>ref()->reactive():{{ refObj }}</h3>
   <button @click="changeRefObj">点击整体赋值</button>
-  <h2>reactive()无法整体赋值:{{ reactiveObj }}</h2>
+  <h3>reactive()无法整体赋值:{{ reactiveObj }}</h3>
   <button @click="changeReactiveObj">点击整体赋值失效</button>
   <button @click="changeReactiveValue">点击只能修改属性值</button>
+  <hr/>
+  <h3>toRef():{{ toRefObj }}</h3>
+  <button @click="changeToRefValue">点击</button>
 
+  <hr/>
   <MyChild fatherData="父数据">
     <template v-slot:childSlot1>
       <div>子组件插槽</div>
     </template>
   </MyChild>
+  <hr/>
 </template>
 
 <script>
   import MyChild from "./pages/MyChild.vue";
-  import {reactive, ref} from "vue";
+  import {reactive, ref, toRef} from "vue";
 
   export default {
     setup() {
+
       let refObj = ref({name: "响应式数据refObj", array: [1, 2, 3]})
       let reactiveObj = reactive({name: "响应式数据reactiveObj", array: [1, 2, 3]})
 
@@ -48,12 +55,28 @@
         console.log(reactiveObj)
       }
 
+
+      let toRefObj = reactive({name: "响应式数据toRefObj", array: [1, 2, 3]})
+      let toRefObj_name = toRef(toRefObj, "name")
+
+      function changeToRefValue() {
+        console.log(toRefObj_name)
+        if (toRefObj.name === "响应式数据toRefObj") {
+          toRefObj_name.value = "响应式数据toRefObj->Change"
+        } else {
+          toRefObj_name.value = "响应式数据toRefObj"
+        }
+        console.log(toRefObj_name)
+      }
+
       return {
         refObj,
         reactiveObj,
+        toRefObj,
         changeRefObj,
         changeReactiveObj,
-        changeReactiveValue
+        changeReactiveValue,
+        changeToRefValue
       }
     },
     components: {
